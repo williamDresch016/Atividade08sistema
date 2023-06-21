8f32b9082c90d0c1c2f0c0069e6aee9842e061f0
williamDresch016 <135863277+williamDresch016@users.noreply.github.com>
2023-06-21 17:21:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ativida08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomes = new List<string>();
+            int buscar = 2;
+            int posicao;
+            posicao = 3;
+            string nome;
+            string novo_usuario;
+            while (true)
+            {
+
+                Console.WriteLine("Cadastro de usúarios\n");
+                Console.WriteLine("1 - Cadastrar");
+                Console.WriteLine("2 - Buscar");
+                Console.WriteLine("3 - Deletar Usuario");
+                Console.WriteLine("4 - Alterar ");
+                Console.WriteLine("Digite Uma Opção");
+                buscar = int.Parse(Console.ReadLine());
+
+                if (buscar ==1)
+                {
+                    Console.WriteLine("Cadastrar um Novo Usuario");
+                    nomes.Add(Console.ReadLine());
+
+                    Console.WriteLine("Usuario Cadastrado com Sucesso");
+
+                }
+
+                if (buscar == 2)
+                {
+                    Console.WriteLine("Digite a Posição que quer buscar: ");
+                    posicao = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("O nome é:"+nomes[posicao]);
+                
+                }
+                if (buscar == 3)
+                {
+                    Console.WriteLine("Digite a posição que deseja deletar");
+
+                    posicao = int.Parse(Console.ReadLine());
+                    nomes.RemoveAt(posicao);
+
+                } 
+                if (buscar == 4)
+                {
+                    Console.WriteLine("Digite o nome de quem quer alterar");
+                    nome = Console.ReadLine();
+                    Console.WriteLine("Digite um novo usuario");
+                    novo_usuario = Console.ReadLine();
+                    posicao = nomes.IndexOf(nome);
+                    nomes[posicao] = novo_usuario;
+                }
+
+
+
+           
+
+            }
+
+
+
+
+        }
+    }
+}
